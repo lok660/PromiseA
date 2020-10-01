c0d8c3426b9ca94199339993b83577cf6ddb05ee
lok660 <lokk7991@gmail.com>
2020-10-02 00:30:11 +0800

    feat:demo调用

---
+++ index.js
@@ -0,0 +1,13 @@
+const Promise = require('./promise')
+
+let p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1)
+  }, 1000);
+})
+
+let t = p.then((data) => {
+  return t
+})
+
+t.then(null, e => console.log(e))
